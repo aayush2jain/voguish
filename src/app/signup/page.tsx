6c3138a24d11a56bfed8c00703f2f648ea3fc769
aayush jain <aayushjain1290@gmail.com>
2024-08-17 23:47:34 +0530

    signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -10,12 +10,17 @@ export default function signup(){
                 <Image src={back} className="h-[100vh] w-[100vw]" alt="" ></Image>
             </div>
 
-            <div className=' w-[30vw] h-[75vh] rounded-3xl flex flex-col absolute top-[10vh] left-[40vw] bg-white' >
+            <div className=' w-[30vw] h-[85vh] rounded-3xl flex flex-col absolute top-[10vh] left-[40vw] bg-white' >
                 <h1 className='mt-[8vh] font-semibold my-4 text-xl mx-[3.5vw] '>SIGN UP</h1>
                 <input type='text' placeholder='Full Name' className='py-[2vh] mb-[2vh] mx-[2.5vw] pl-4 rounded-xl bg-gray-200'></input>
                 <input type='text' placeholder='Email' className='py-[2vh] mb-[2vh] mx-[2.5vw] pl-4 rounded-xl bg-gray-200'></input>
+                    <div className=' mb-[2vh] mx-[2.5vw] pl-4 rounded-xl bg-gray-200 flex flex-row'>
+                        <h1 className='py-[2vh]'>+91</h1>
+                        <input type='text' placeholder='Enter Your Mobile' className='ml-[1vw] py-[2vh] focus:outline-none bg-gray-200'></input>
+
+                    </div>
                 <input type='text' placeholder='Password' className='py-[2vh] mb-[2vh] mx-[2.5vw] pl-4 rounded-xl bg-gray-200'></input>
-                <button className='width-[10vw] mx-[2.5vw] py-3 bg-black text-white text-xl rounded-xl'>Sign up</button>
+                <button className='width-[10vw] mx-[2.5vw] py-3 bg-black text-white text-xl rounded-3xl'>Get OTP</button>
                 <div className='flex flex-row rounded-xl border-black border-2 w-[25vw] ml-[2.5vw] mt-2'>
                     <div className=" mt-6 ml-[5vw]">
                         <img src='https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg' alt=''></img>
